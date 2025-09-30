0220f86683cd8618ec16728b7516a566e3f7671e
Alanoud Altuwaijri <143048192+alanoudtw@users.noreply.github.com>
2025-10-01 02:31:48 +0300

    Update download_model.py

--- download_model.py
+++ download_model.py
@@ -7,9 +7,9 @@ os.environ["HF_HUB_DISABLE_SYMLINKS_WARNING"] = "1"
 os.environ["SPEECHBRAIN_LOCAL_DOWNLOAD_STRATEGY"] = "copy"
 
 # Download and save the model
-classifier = EncoderClassifier.from_hparams(
+EncoderClassifier.from_hparams(
     source="speechbrain/emotion-recognition-wav2vec2-IEMOCAP",
     savedir="pretrained_models/emotion"
 )
 
-print("✅ Model downloaded successfully!")
+print("✅ Model downloaded into pretrained_models/emotion")
