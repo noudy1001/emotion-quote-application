d30b065394ba605c967711513b96e53a1662e6d6
Alanoud Altuwaijri <143048192+alanoudtw@users.noreply.github.com>
2025-10-01 04:24:57 +0300

    Update app.py

--- app.py
+++ app.py
@@ -83,4 +83,6 @@ def infer_audio():
         except: pass
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Use Render's port if available
+    app.run(host="0.0.0.0", port=port, debug=True)
+
