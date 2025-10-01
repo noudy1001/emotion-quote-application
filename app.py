3e64b7fb21a6bcc7691477cd840e2e1b10b6b51d
Alanoud Altuwaijri <143048192+alanoudtw@users.noreply.github.com>
2025-10-01 15:39:12 +0300

    Update app.py

--- app.py
+++ app.py
@@ -83,5 +83,5 @@ def infer_audio():
         except: pass
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
 
